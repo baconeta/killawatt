ee12ae07e36766f03e5856c30350b3a6833b596e
Josh Villgren <joshuavillgren@gmail.com>
2024-07-10 20:19:00 +1200

    add some more form data options

--- csvuploader/forms.py
+++ csvuploader/forms.py
@@ -3,3 +3,16 @@ from django import forms
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
+
+    PROVIDER_CHOICES = [
+        ('provider_a', 'Flick'),
+        ('provider_b', 'Meridian'),
+        ('provider_c', 'Mercury'),
+    ]
+    provider = forms.ChoiceField(choices=PROVIDER_CHOICES, label='Select your current provider')
+
+    includes_GST = forms.BooleanField(label="Includes GST")
+
+    def __init__(self, post, files):
+        super().__init__()
+        self.fields['includes_GST'].initial = True
--- csvuploader/views.py
+++ csvuploader/views.py
@@ -14,12 +14,17 @@ def upload_file(request):
         form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             file = request.FILES['file']
+            provider = form.cleaned_data['provider']
+            gst_inclusive = form.cleaned_data['includes_GST']
             data = pd.read_csv(file)
+
             # Store the DataFrame in the session or pass it to the next function/page
             request.session['data'] = data.to_json()
-            return redirect('show_data')
+            request.session['provider'] = provider
+            request.session['gst_inclusive'] = gst_inclusive
+        return redirect('show_data')
     else:
-        form = UploadFileForm()
+        form = UploadFileForm(request.POST, request.FILES)
     return render(request, 'upload.html', {'form': form})
 
 
@@ -28,6 +33,7 @@ def show_data(request):
     if data_json:
         data = pd.read_json(StringIO(data_json))
 
+        # TODO this should be handled by provider
         # Example manipulation: adding a column with prices for different providers
         providers = {
             'Provider A': 0.15,
@@ -35,6 +41,7 @@ def show_data(request):
             'Provider C': 0.20
         }
 
+        # TODO this should collate off peak and peak time data for comparisons across providers
         for provider, price in providers.items():
             data[provider] = data['value'] * price
 
